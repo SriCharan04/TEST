943aad0fcfc1a558e06acb16fd4a3c3085ff2f06
SriCharan04 <tsricharan04@gmail.com>
2023-06-12 21:32:40 +0530

    Further modified this file

--- assn3a.c
+++ assn3a.c
@@ -3,21 +3,24 @@
 #include <time.h>
 #include <math.h>
 #include <GL/glut.h>
+#include <GL/gl.h>
+#include <GL/glu.h>
 
 float PI = 4*atan(1); /*This pi is used only for graphics purpose, and not misused to manipulate end results*/
 int radius;
 int times;
 int inside = 0;
 int outside = 0;
+int random_points[1000][2];
 
 void geometry()
 {
 	glClear(GL_COLOR_BUFFER_BIT);
 	glBegin(GL_LINE_LOOP);
-	glVertex2f(-50,-50);
-	glVertex2f(-50,50);
-	glVertex2f(50,-50);
-	glVertex2f(50,50);
+	glVertex2f(-150,-150);
+	glVertex2f(-150,150);
+	glVertex2f(150,150);
+	glVertex2f(150,-150);
 	glEnd();
 
 	glBegin(GL_LINE_LOOP);
@@ -32,25 +35,20 @@ void display(int argc, char** argv)
 {
 	glutInit(&argc,argv);
 	glutInitDisplayMode(GLUT_RGB);
-
-	glutInitWindowSize(200,200);
+	glutInitWindowSize(600,600);
 	glutInitWindowPosition(0,0);
 	glutCreateWindow("Simple Diagram");
-	gluOrtho2D(-50,50,-50,50);
-
+	gluOrtho2D(-300,300,-300,300);
 	glutDisplayFunc(geometry);
 	glutMainLoop();
 }
 
 void input_radius()
 {
-	printf("%s","Give a valid radius, less than 100: ");
+	printf("%s","Give a valid radius, less than 150: ");
 	scanf("%i", &radius);
 
-	printf("%s","Enter the number of times, the simulation is to be done (Give some large numbers for better results)\n");
-	scanf("%i",&times);
-
-	if (radius > 100.001)
+	if (radius > 150)
 	{
 		printf("%s","Please enter a valid radius\n");
 		exit(-1);
@@ -59,8 +57,6 @@ void input_radius()
 
 int main(int argc, char** argv)
 {
-	display(argc, argv);
-	int random_points[1000][2];
 	srand(time(NULL));
 	input_radius();
 	for (int i=0;i <= 999; i++)
@@ -89,4 +85,6 @@ int main(int argc, char** argv)
 	printf("%s","But the theoretical ratio is (pi*r^2)/(a^2-pi*r^2)\n");
 	double pi = (ratio*10000)/((ratio+1)*(radius^2));
 	printf("Therefore the estimated value of pi through this monte Carlo Simulation is %f \n", pi);
+	printf("This Graphic object depicts the position of points in the circle and square (feel free to use Alt + F4, to close the window object)\n");
+	display(argc, argv);
 }
