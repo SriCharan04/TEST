3cd80d8b94b5bb2f00083c466fe991749fe0474c
SriCharan04 <tsricharan04@gmail.com>
2023-06-12 19:45:07 +0530

    modified assn3 task 1

--- assn3a.c
+++ assn3a.c
@@ -2,11 +2,45 @@
 #include <stdlib.h>
 #include <time.h>
 #include <math.h>
+#include <GL/glut.h>
 
+float PI = 4*atan(1); /*This pi is used only for graphics purpose, and not misused to manipulate end results*/
 int radius;
 int times;
 int inside = 0;
 int outside = 0;
+
+void geometry()
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	glBegin(GL_LINE_LOOP);
+	glVertex2f(-50,-50);
+	glVertex2f(-50,50);
+	glVertex2f(50,-50);
+	glVertex2f(50,50);
+	glEnd();
+
+	glBegin(GL_LINE_LOOP);
+	for (int k = 0;k < 1000;k++)
+	{
+		glVertex2f(radius*cos(PI/500*k),radius*sin(PI/500*k));
+	}
+	glEnd();
+	glFlush();
+}
+void display(int argc, char** argv)
+{
+	glutInit(&argc,argv);
+	glutInitDisplayMode(GLUT_RGB);
+
+	glutInitWindowSize(200,200);
+	glutInitWindowPosition(0,0);
+	glutCreateWindow("Simple Diagram");
+	gluOrtho2D(-50,50,-50,50);
+
+	glutDisplayFunc(geometry);
+	glutMainLoop();
+}
 
 void input_radius()
 {
@@ -23,8 +57,9 @@ void input_radius()
 	}
 }
 
-int main()
+int main(int argc, char** argv)
 {
+	display(argc, argv);
 	int random_points[1000][2];
 	srand(time(NULL));
 	input_radius();
@@ -45,12 +80,13 @@ int main()
 		{
 			++outside;
 		};
+	printf("The current status of inside and outside are %i and %i\n",inside, outside);
 	}
 	/*We have area_in/area_out = (pi*r^2)/(a^2-pi*r^2)*/
-	
-	double ratio = inside/outside;
+	printf("The current status of inside and outside are %i and %i\n",inside, outside);
+	float ratio = inside/outside;
 	printf("The experimental ratio is %f \n",ratio);
 	printf("%s","But the theoretical ratio is (pi*r^2)/(a^2-pi*r^2)\n");
-	double pi = (ratio*10000)/((ratio+1)*(1 << radius));
+	double pi = (ratio*10000)/((ratio+1)*(radius^2));
 	printf("Therefore the estimated value of pi through this monte Carlo Simulation is %f \n", pi);
 }
